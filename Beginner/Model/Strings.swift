8f0e0cf97df9e1c5069c5fc7f30b3ae226f48c6b
David Oliver Doswell <davidoliverdoswell@davids-macbook-air.local>
2017-12-31 16:40:15 -0500

    Fixed 'Antonopoulos' and other typos

--- Beginner/Model/Strings.swift
+++ Beginner/Model/Strings.swift
@@ -25,7 +25,7 @@ struct ArrayStrings {
         ["HashPower Documentary", "Noded", "The Bitcoin Podcast", "Unchained"],
         ["Bitseed", "Blockstream Satellite", "Halong Mining", "Lightning Labs", "Open Bazaar"],
         ["Coin Center"],
-        ["Andreas Antonopolous", "Off Chain with Jimmy Song", "The Bitcoin News Show"]
+        ["Andreas Antonopoulos", "Off Chain with Jimmy Song", "The Bitcoin News Show"]
     ]
 }
 
@@ -33,15 +33,15 @@ struct DetailArrayStrings {
     let detailArray = [
         ["Creator of Bitcoin"],
         ["Getting started", "Blockchain mechanics and video series", "Step-by-step YouTube video on how blockchain works", "Foundational Bitcoin resource", "Presentation on antifragility", "Early emails, code and quotations"],
-        ["Hunter S. Thompson-style retelling of Bitcoin's early days", "Foundational book on getting started with Bitcoin", "Two smart journalists cover Bitcoin around the world", "The economics of Bitcoin deftly explained", "Andreas Antonopolous delves into the why of Bitcoin"],
-        ["Hardware wallet founded by a Bitcoin Core developer", "Well-designed hardware wallet for your home computer", "Stylish and multi-currency alternative for hardware wallets", "Cyberpunk Bitcoin bearer instrument in a USB stick", "The original & most secure hardware wallet"],
+        ["Hunter S. Thompson-style retelling of Bitcoin's early days", "Foundational book on getting started with Bitcoin", "Two smart journalists cover Bitcoin around the world", "The economics of Bitcoin deftly explained", "Andreas Antonopoulos delves into the why of Bitcoin"],
+        ["Hardware wallet founded by a Bitcoin Core developer", "Well-designed hardware wallet for your home computer", "Stylish and multi-currency alternative for hardware wallets", "Cyberpunk Bitcoin bearer instrument on a USB stick", "The original & most secure hardware wallet"],
         ["Riga, Latvia", "Paris, France", "Prague, Czech Republic", "Stanford, California"],
         ["Developer Documentation", "Bitcoin Core mailing list", "Propose solutions to improve the core protocol", "Essays on technical topics", "Two-week program in NYC taught by Core developers", "Intense and highly technical seminar taught by Jimmy Song"],
         ["The first decentralized exchange", "One of the oldest and most respected crypto exchanges", "A preferred platform for crypto day traders", "Meet up to buy or sell Bitcoin offline in a public place", "Buy Bitcoin easily with a debit card"],
         ["Simple, secure online desktop wallet", "Hip starter wallet for non-technical users", "Preferred Segwit wallet for security-conscious users", "Sleek Segwit wallet with fee estimator"],
-        ["Stylish, well-produced doc in a three-part series", "Technical podcast on economics and monetary policy", "Long-running podcast on the Bitcoin ecosystem", " A Forbes contributor interviews the Bitcoin community"],
+        ["Stylish, well-produced podcast in a three-part series", "Technical podcast on economics and monetary policy", "Long-running podcast on the Bitcoin ecosystem", " A Forbes contributor interviews the Bitcoin community"],
         ["Plug and play full node for your home or apartment", "Bitcoin blockchain transactions securely from space","Handsome miner built by a Bitcoin Core developer", "Open protocol layer for scaling blockchains", "Cool peer-to-peer marketplace for buyers and sellers"],
         ["Non-profit research and advocacy center"],
-        ["World-renowned Bitcoin educator, author and hacker", "Daily news show hosted by Jimmy Song and Tone Vays", "Weekly cypherpunk hangout with special guests"]
+        ["World-renowned Bitcoin educator, author and activist", "Daily news show hosted by Jimmy Song and Tone Vays", "Weekly cypherpunk hangout with special guests"]
     ]
 }
